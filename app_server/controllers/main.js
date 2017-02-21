e70803a1cae5bb28bfab3f5070f3841f472a8945
susana <susanafotu07@gmail.com>
2017-02-21 13:43:44 -0800

    changes to register page

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -3,6 +3,10 @@ module.exports.index = function(req, res){
   res.render('index', { title: 'QuikRent' });
 };
 
+module.exports.register = function(req, res){
+  res.render('register', { title: 'Register for QuikRent' });
+};
+
 module.exports.login = function(req, res){
-  res.render('login', { title: 'QuikRent Register or Login' });
+  res.render('login', { title: 'Log into QuikRent' });
 };
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -4,6 +4,7 @@ var ctrlMain = require('../controllers/main');
 
 /* GET home page. */ 
 router.get('/', ctrlMain.index);
+router.get('/register', ctrlMain.register);
 router.get('/login', ctrlMain.login);
 
 module.exports = router;
