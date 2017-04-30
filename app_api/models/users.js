8e9737f85cbad43b7f8e1c759daf08eb00f346bb
susanafotu <susanafotu07@gmail.com>
2017-04-30 00:40:25 -0700

    updated search schema

--- app_api/models/users.js
+++ app_api/models/users.js
@@ -4,16 +4,36 @@ var jwt = require('jsonwebtoken');
 //var passportLocalMongoose = require('passport-local-mongoose');
 //defining the searches schema
 var searchSchema = new mongoose.Schema({
-    //I think we have to do some belongs to type of syntax but I'm not sure
-    
-    //how to require the userSchema?
-    //user: [userSchema.schema], //user data so we know who the search belongs to
-    minPrice: String,
-    maxPrice: String,
-    //slackToken: String, //i don't think we need the slacktoken in the search because it should be saved in the userSchema
-    bedrooms: String,
-    bathrooms: String
-    //coords: {type: [Number], index: '2dshere'} for when we allow coordinates
+    craigslist_housing_section:{
+        type:String,
+        required:true
+    },
+    craigslist_site:{
+        type:String,
+        required:true
+    },
+    areas:String, //how to denote array
+    min_price:{
+        type:String,
+        required:true
+    },
+    max_price:{
+        type:String,
+        required:true
+    },
+    bed:{
+        type:String,
+        required:true
+    },
+    bath:{
+        type:String,
+        required:true
+    },
+    slack_token:{
+        type:String,
+        unique: true,
+        required:true
+    }
 });
 
 
